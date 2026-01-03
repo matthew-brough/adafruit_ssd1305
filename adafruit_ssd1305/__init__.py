8bff9c875ff4ad3d88b8f09cf3958dce9b672a64
artemOP <26147889+artemOP@users.noreply.github.com.>
2026-01-03 21:17:58 +0000

    include the baseclass for typing

--- adafruit_ssd1305/__init__.py
+++ adafruit_ssd1305/__init__.py
@@ -1,4 +1,4 @@
 from . import constants
-from .adafruit_ssd1305 import SSD1305_128x32, SSD1305_128x64
+from .adafruit_ssd1305 import SSD1305, SSD1305_128x32, SSD1305_128x64
 
-__all__ = ["constants", "SSD1305_128x32", "SSD1305_128x64"]
+__all__ = ["constants", "SSD1305", "SSD1305_128x32", "SSD1305_128x64"]
